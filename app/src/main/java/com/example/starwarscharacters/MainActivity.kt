0cb06b2d0ea4925e37e6a084f4395675c2d05f56
Mostafa Hussien <mostafa.hussien@swvl.app>
2022-05-19 15:58:58 +0200

    init dependencies

---
+++ app/src/main/java/com/example/starwarscharacters/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.starwarscharacters
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModel
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
